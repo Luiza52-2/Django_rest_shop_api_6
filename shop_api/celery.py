5fd6d13d0ace7a9e9ae5fe706b14fdb5a40e12c0
Luiza52-2 <nasykulova19@gmail.com>
2025-07-26 21:32:23 +0600

    Added tasks

--- shop_api/celery.py
+++ shop_api/celery.py
@@ -2,6 +2,7 @@ import os
 
 from celery import Celery
 from dotenv import load_dotenv
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
@@ -19,3 +20,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
+app.conf.beat_schedule = {
+    'send_daily_report': {
+        'task': 'users.tasks.send_daily_report',
+        'schedule': crontab(hour=0, minute=0)
+    },
+}
--- users/tasks.py
+++ users/tasks.py
@@ -1,8 +1,29 @@
 from celery import shared_task
 import time
+from datetime import datetime, timedelta
+import os
 
 @shared_task
 def sent_otp_email(user_email, code):
     print('Sending...')
     time.sleep(20)
     print('Email sent!')
+
+@shared_task
+def send_daily_report():
+    print('Sending daily report...')
+    # Logic to send the daily report
+    time.sleep(50)
+    print('Daily report sent!')
+
+@shared_task
+def cleanup_old_reports():
+    folder = "/tmp/"
+    cutoff = datetime.now() - timedelta(days=7)
+    for filename in os.listdir(folder):
+        if filename.endswith(".txt"):
+            path = os.path.join(folder, filename)
+            mtime = datetime.fromtimestamp(os.path.getmtime(path))
+            if mtime < cutoff:
+                os.remove(path)
+                print(f"Deleted old report {filename}")
